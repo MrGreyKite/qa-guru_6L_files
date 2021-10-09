9e630b455f9e9ce115cc63c4492d8d61a9421b3d
Рудольф Багмет <nolde3@yandex.ru>
2021-10-10 03:59:05 +0400

    added test for downloaded CSV file

--- src/test/java/files/bagmet/TestsForFiles.java
+++ src/test/java/files/bagmet/TestsForFiles.java
@@ -1,15 +1,25 @@
 package files.bagmet;
 
+import com.opencsv.CSVReader;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.contentOf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestsForFiles {
@@ -28,6 +38,29 @@ public class TestsForFiles {
         //assertEquals("Cambria", titleRun.getFontFamily()); - выдает результат, что fontFamily равно null
         assertThat(paragraphs.get(5).getRuns().get(0).getFontFamily()).isEqualTo("Arial"); //возможно, из-за текста на русском?
         assertThat(paragraphs.get(5).getText()).contains("Text in English");
+    }
 
+    @Test
+    void txtFileTest() throws NullPointerException {
+        File textFile = new File(getClass().getClassLoader().getResource("text.txt").getFile());
+        assertThat(contentOf(textFile)).contains("Java automation");
+        assertThat(contentOf(textFile)).endsWith(".");
+    }
+
+    @Test
+    void downloadCVS() throws Exception {
+        open("https://sample-videos.com/download-sample-csv.php");
+        File downloadedCSV = $("a[data='1']").download();
+        try (CSVReader reader = new CSVReader(new FileReader(downloadedCSV))) {
+            List<String[]> strings = reader.readAll();
+            assertThat(strings).contains(
+                    new String[] {"1", "Eldon Base for stackable storage shelf, platinum",
+                            "Muhammed MacIntyre","3",
+                            "-213.25","38.94","35","Nunavut","Storage & Organization","0.8"},
+                    new String[]{"10","Xerox 198","Dorothy Badders","678",
+                            "-226.36","4.98","8.33","Nunavut","Paper","0.38"}
+            );
+
+        }
     }
 }
